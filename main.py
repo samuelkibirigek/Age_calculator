4d93a2e26a4bdc3470e9649e4926082c9b49e28b
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-01-01 18:19:15 +0300

    Age calculator Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+"""
+    This Program assumes one lives upto 90 years of age to
+    tell how much time you have left to live in days, weeks and months.
+    Also it will tell how long you have live in weeks, and months.
+"""
+
+print("NOTE: This program assumes we all live upto 90 years of age.")
+
+age = int(input("Kindly enter your current age: "))
+
+
+def time(years):
+    days = int(years * 365)
+    weeks = int(years * 52)
+    months = int(years * 12)
+    return f"{days} days, {weeks} weeks and {months} months"
+
+
+word = time(age)
+
+print(f"You have currently lived for {word}.\n")
+
+time_left = 90 - age
+left = time(time_left)
+
+print(f"You have {left} left to live.")
